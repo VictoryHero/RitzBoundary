19b033ac5a46045edbb5734379a569df81c94c6d
刘波 <liubo17@lzu.edu.cn>
2020-12-09 18:46:16 +0800

    update method

--- main_ritz2.m
+++ main_ritz2.m
@@ -1,16 +1,15 @@
 tic;
-%% 两点边值问题
+%% 求边界值
 clear all;
 clc;
 close all;
 
 %% 正式求解
 
-for NumM=5:8
 p=1;q=0;
 high=1;low=-1;
 
-N=NumM;
+N=50;
 X=linspace(low,high,N+1);
 f=inline('sin(pi*x)-cos(pi*x)','x');
 exact_u=(sin(pi*X)-cos(pi*X))/pi^2+(4*pi-1)*X/(3*pi)-(pi^2-pi+1)/pi^2;
@@ -25,18 +24,17 @@ K(1,1)=K(1,1)-1;
 
 b(end,1)=b(end,1)+2;         
 K(end,end)=K(end,end)+2;       
-
+b(:)=b(:);
 
 u=[2.*K\b];
 exact_u=exact_u';
 grid on;
-subplot(4,2,(NumM-5)*2+1);
+subplot(1,2,1);
 plot(X,u,'ro-',X,exact_u,'b^:');
 title('Numerical solutions vs Exact solutions');
-legend('Numerical_solution','Exact_solution','Location','NorthWest');
-subplot(4,2,(NumM-5)*2+2);
+legend('Numerical_solution','Exact_solution');
+subplot(1,2,2);
 plot(X,u-exact_u,'b x-');
 legend('error_solution');
 title('error');
-end
 toc;
--- rightVector.m
+++ rightVector.m
@@ -1,13 +1,9 @@
 function b=rightVector(f,x,h,n,low,high)
-b=zeros(n,1);
+b=zeros(n+1,1);
 for i=2:n+1
-% fun1=@(kesai) f((x(i)+h.*kesai)).*(1-kesai)+f((x(i-1)+h.*kesai)).*kesai;
-% b(i-1)=h.*quad(fun1,low,high);
-
-fun1=@(kesai) f(x(i-1)+h.*kesai).*(1-kesai);
-fun2=@(kesai) f(x(i-1)+h.*kesai).*kesai;
+fun1=@(kesai) f(x(1,i-1)+h.*kesai).*(1-kesai);
+fun2=@(kesai) f(x(1,i-1)+h.*kesai).*kesai;
 b(i-1)=b(i-1)+h.*quad(fun1,low,high);
 b(i)=h.*quad(fun2,low,high);
 end
-%b=b(2:end);   %去掉了第一个分量
 
